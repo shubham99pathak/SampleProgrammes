a3741ba4a2a01b42b58958595f61646df1bfce12
Shubham Pathak <55439749+shubham99pathak@users.noreply.github.com>
2021-05-13 19:25:29 +0530

    Create welcome.cpp

---
+++ C++/welcome.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    char str[5];
+    cout<<"Enter 'World'=";
+    cin>>str;
+    cout<<"Welcome to C++ "<<str<<".";
+    getch();
+}
