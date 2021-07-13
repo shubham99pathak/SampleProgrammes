b7c146f7999380f7402bc6cb565cee887cd42c43
Shubham Pathak <55439749+shubham99pathak@users.noreply.github.com>
2021-07-13 19:32:47 +0530

    Create javawelcome.java

---
+++ Java/javawelcome.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class javawelcome
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter language: ");
+        String ch= sc.next();
+        System.out.println("...Welcome to "+ch+" World...");
+    }
+}
