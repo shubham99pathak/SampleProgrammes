a194dc7f2698daeb13a7db8a01c359509cc35716
Shubham Pathak <55439749+shubham99pathak@users.noreply.github.com>
2021-07-13 19:31:06 +0530

    Create cppwelcome.cpp

---
+++ C++/cppwelcome.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch[3];
+    cout<<"Enter language: ";
+    cin>>ch;
+    cout<<"...Welcome to "<<ch<<" World...";
+    return 0;
+}
